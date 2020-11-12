00798aa60820579888eebfbb7f90ede798180212
vladvinnikov <vladvinnikov@gmail.com>
2020-11-12 17:05:17 +0200

    Update chewy version

--- lib/chewy/runtime.rb
+++ lib/chewy/runtime.rb
@@ -3,7 +3,7 @@ require 'chewy/runtime/version'
 module Chewy
   module Runtime
     def self.version
-      Thread.current[:chewy_runtime_version] ||= Version.new(Chewy.client.info['version']['number'])
+      Thread.current[:chewy_runtime_version] ||= Version.new("7.0.10")
     end
   end
 end
